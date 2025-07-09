e74828f1e7728ffc1ced4e353f2cd1fe49b74200
Tom LaMarre <tlamarre91+github@gmail.com>
2025-07-08 19:46:17 -0500

    placeholder

--- ingest/model/shelter_info.py
+++ ingest/model/shelter_info.py
@@ -1,11 +1,13 @@
 from dataclasses import dataclass
 
+from model.pet_friendly_category import PetFriendlyCategory
 from model.shelter_category import ShelterCategory
 
 
 @dataclass
 class ShelterInfo:
     name: str
+    email: str
     address_line1: str
     address_line2: str
     city: str
