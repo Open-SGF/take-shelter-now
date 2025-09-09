2a38c634a36d61357796880beafe995b6c62e2fc
DaltonWemer <daltonwemer.22@gmail.com>
2025-09-09 15:44:31 -0500

    used shadcn button to get location

--- src/lib/components/ui/GetLocation/GetLocation.svelte
+++ src/lib/components/ui/GetLocation/GetLocation.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { hasLocation } from '$lib/stores/global';
+	import { Button } from '$lib/components/ui/Button'
 
 	function enableLocation() {
 		hasLocation.set(true);
@@ -7,15 +8,15 @@
 </script>
 
 <div>
-	<button
-		on:click={enableLocation}
+	<Button 
+		onclick={() => {enableLocation()}}
 		class="flex items-center justify-center w-full h-[58px] rounded-lg bg-[#0892d2] text-white"
-	>
+	>		
 		<img
 			src="https://res.cloudinary.com/du9tnv8ss/image/upload/v1756782708/navigation_bgtfde.png"
 			alt=""
 			class="w-6 h-6 mr-2"
 		/>
 		Current Location
-	</button>
+	</Button>
 </div>
