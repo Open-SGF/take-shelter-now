8aacd02b223343b2d32ddfe9b5e124d897e047ed
DaltonWemer <daltonwemer.22@gmail.com>
2025-09-09 16:29:30 -0500

    ran prettier

--- src/lib/components/ui/GetLocation/GetLocation.svelte
+++ src/lib/components/ui/GetLocation/GetLocation.svelte
@@ -7,14 +7,14 @@
 </script>
 
 <div>
-	<button 
+	<button
 		on:click={enableLocation}
-		class="flex items-center justify-center w-full h-[58px] rounded-lg bg-[#0892d2] text-white"
-	>		
+		class="flex h-[58px] w-full items-center justify-center rounded-lg bg-[#0892d2] text-white"
+	>
 		<img
 			src="https://res.cloudinary.com/du9tnv8ss/image/upload/v1756782708/navigation_bgtfde.png"
 			alt=""
-			class="w-6 h-6 mr-2"
+			class="mr-2 h-6 w-6"
 		/>
 		Current Location
 	</button>
