dc90dad1082db05804d282e1a0a1cc58701e253b
shane <sm0862512@otc.edu>
2026-01-13 19:55:55 -0600

    feat: add Shelter interface and `shelters` store for managing shelter data

--- src/lib/stores/global.ts
+++ src/lib/stores/global.ts
@@ -1,7 +1,21 @@
 import { writable } from 'svelte/store';
 
+export interface Shelter {
+	Name: string;
+	'Address Line 1': string;
+	'Address Line 2': string;
+	City: string;
+	State: string;
+	Zip: string;
+	Latitude: string;
+	Longitude: string;
+}
+
 // Tracks whether user has allowed location access
 export const hasLocation = writable(false);
 
 // Stores user's current location
 export const userLocation = writable<{ latitude: number; longitude: number } | null>(null);
+
+// Stores all shelters with distance calculations
+export const shelters = writable<Array<Shelter & { distance: number }>>([]);
