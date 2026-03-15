e38491f281fa993b714dc5e523d099400e953319
Levi Zitting <me@levizitting.com>
2026-03-15 15:05:39 -0500

    fix: add setup-server file for vitest

--- vite.config.ts
+++ vite.config.ts
@@ -66,6 +66,7 @@ export default defineConfig({
 					environment: 'node',
 					include: ['src/**/*.{test,spec}.{js,ts}'],
 					exclude: ['src/**/*.svelte.{test,spec}.{js,ts}'],
+					setupFiles: ['./vitest/setup-server.ts'],
 				},
 			},
 			{
