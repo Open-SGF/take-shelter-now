4ada88e6ce51e65de2b7f592bb5c26b83bb06ac4
Levi Zitting <me@levizitting.com>
2026-03-17 10:10:57 -0500

    chore: remove unneeded id in shelter list

--- src/lib/components/shelters/ShelterList/ShelterList.svelte
+++ src/lib/components/shelters/ShelterList/ShelterList.svelte
@@ -5,7 +5,7 @@
 	const shelterState = getShelterStateContext();
 </script>
 
-<div id="shelter_list" data-testid="shelter-list">
+<div data-testid="shelter-list">
 	{#if shelterState.dataState.kind === 'loading'}
 		<div class="space-y-3" data-testid="shelter-list-loading">
 			<p class="text-sm text-slate-600">Loading nearby shelters...</p>
