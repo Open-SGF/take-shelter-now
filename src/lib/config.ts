7bdd24c626452d0eed80a461db795ddc406d220c
Jared Bates <me@jaredtbates.com>
2026-03-17 20:44:50 -0500

    attempt v3

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,3 @@
-import { env } from '$env/dynamic/public';
-
 export const config = {
-	sheltersJsonUrl: env?.PUBLIC_SHELTERS_JSON_URL || '/shelters.json',
+	sheltersJsonUrl: import.meta.env.PUBLIC_SHELTERS_JSON_URL || '/shelters.json',
 };
