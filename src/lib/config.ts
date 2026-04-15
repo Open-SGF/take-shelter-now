db63d2cbb9f7fdb847953516b50aba675d625696
Levi Zitting <me@levizitting.com>
2026-04-14 20:39:27 -0500

    feat: add robots.txt

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,3 +1,5 @@
 export const config = {
+	siteEnv: import.meta.env.PUBLIC_SITE_ENV || 'development',
 	sheltersJsonUrl: import.meta.env.PUBLIC_SHELTERS_JSON_URL || '/shelters.json',
+	allowIndexing: import.meta.env.PUBLIC_SITE_ENV === 'production',
 };
