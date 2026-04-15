0cfa6030c75bdfaacd7c5d9cd98f68718e0dff5c
Levi Zitting <me@levizitting.com>
2026-04-14 20:44:21 -0500

    feat: add sitemap.xml

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,8 @@
+import { env } from '$env/dynamic/public';
+
 export const config = {
-	siteEnv: import.meta.env.PUBLIC_SITE_ENV || 'development',
-	sheltersJsonUrl: import.meta.env.PUBLIC_SHELTERS_JSON_URL || '/shelters.json',
-	allowIndexing: import.meta.env.PUBLIC_SITE_ENV === 'production',
+	siteEnv: env.PUBLIC_SITE_ENV || 'development',
+	siteUrl: __SITE_URL__,
+	sheltersJsonUrl: env.PUBLIC_SHELTERS_JSON_URL || '/shelters.json',
+	allowIndexing: env.PUBLIC_SITE_ENV === 'production',
 };
